8da0378b43747ab840d221b9d30d0c7704240ce3
Ash-x-o <ashikoshi123@gmail.com>
2025-12-14 14:44:39 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/*path", (req, res) => {
     res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
   });
 } else {
